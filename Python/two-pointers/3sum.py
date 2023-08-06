b9cc2938ada3fa4b05abfd50f6682dc0feb8cd0f
Dilan Tern <dtern98@gmail.com>
2023-08-05 20:42:54 -0700

    Added Python solution for 3sum.py

---
+++ Python/two-pointers/3sum.py
@@ -0,0 +1,39 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        result = []
+        n = len(nums)
+
+        if nums[0] > 0 or nums[-1] < 0 or (nums[0] + nums[1] + nums[2] > 0) or (nums[-1] + nums[-2] + nums[-3] < 0):
+            return result
+        for i in range(len(nums)):
+            # Avoiding duplicates
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            left = i + 1
+            right = n - 1
+
+            while left < right:
+                total_sum = nums[i] + nums[left] + nums[right]
+
+                if total_sum == 0:
+                    result.append([nums[i], nums[left], nums[right]])
+
+                    left += 1
+                    right -= 1
+
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+
+                elif total_sum < 0:
+                    left += 1
+
+                else:
+                    right -= 1
+
+        return result
+
